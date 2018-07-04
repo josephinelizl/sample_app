cd8442035f4f8ff4652df4ab12893b595d43c8f9
josephinelizl <josephine.marquez@appstone.tech>
2018-07-04 18:32:15 +0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 																		
 	has_secure_password
 	
-	validates :password, presence: true, length: { minimum: 5 }
+	validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
 	
 	# Returns the hash digest of the given string.
 	def User.digest(string)
