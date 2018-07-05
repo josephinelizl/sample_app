67a36c90f1ee309619762916a835f72e686c7f1d
josephinelizl <josephine.marquez@appstone.tech>
2018-07-05 18:01:48 +0800

    Update in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_methd = :smtp
-   host = 'http://floating-lowlands-46990.herokuapp.com/'
+   host = 'https://floating-lowlands-46990.herokuapp.com/'
    config.action_mailer.default_url_options = { host: host }
    ActionMailer::Base.smtp_settings = {
    		:address				=> 'smtp.sendgrip.net',
